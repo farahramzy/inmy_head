7b891c3b470d41e36c5082ee7a68629cb564ef0c
haniensami <76820916+haniensami@users.noreply.github.com>
2022-10-29 21:34:40 +0200

    Listview worry

---
+++ lib/worry/worry.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:inmy_head/worry/worry_data.dart';
+
+import '../constants.dart';
+import '../navigation_drawer/drawer.dart';
+// import 'package:inmy_head/reflections/reflections2.dart';
+// import 'constants.dart';
+// import 'navigation_drawer/drawer.dart';
+
+class Worry extends StatefulWidget {
+  const Worry({super.key});
+  @override
+  State<Worry> createState() => _WorrysState();
+}
+
+// class _WorrysState extends State<Worry> {
+//   final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Container(
+//         decoration: const BoxDecoration(
+//             image: DecorationImage(
+//                 image: AssetImage("images/reflect.png"), fit: BoxFit.cover)),
+//         child: Scaffold(
+//           key: _globalKey,
+//           drawer: const NavigationDrawer(),
+//           backgroundColor: Colors.transparent,
+//           body: PageView(
+//             children: <Widget>[
+//               // Padding(
+//               //   padding: const EdgeInsets.all(15.0),
+//               //   child: Row(
+//               //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               //     children: [
+//               //       IconButton(
+//               //         onPressed: () {
+//               //           _globalKey.currentState?.openDrawer();
+//               //         },
+//               //         icon: const Icon(Icons.menu, size: FontSize.s40),
+//               //         color: ColorManager.white,
+//               //       ),
+//               //       IconButton(
+//               //         onPressed: () {
+//               //           Navigator.pushNamed(context, 'homePage');
+//               //         },
+//               //         icon: const Icon(Icons.close_sharp, size: FontSize.s40),
+//               //         color: ColorManager.white,
+//               //       ),
+//               //     ],
+//               //   ),
+//               // ),
+//               Stack(
+//                 children: const <Widget>[
+//                   Cont1(),
+//                   TextPage(text: "Is it that serious?"),
+//                 ],
+//               ),
+//               Stack(
+//                 children: const <Widget>[
+//                   Cont1(),
+//                   TextPage(text: "Is there a solution?"),
+//                 ],
+//               ),
+//               Stack(
+//                 children: const <Widget>[
+//                   Cont1(),
+//                   TextPage(text: "Is it out of your control?"),
+//                 ],
+//               ),
+//               Stack(
+//                 children: const <Widget>[
+//                   Cont1(),
+//                   TextPage(text: "Have you overcome worse?"),
+//                 ],
+//               ),
+//               Stack(
+//                 children: const <Widget>[
+//                   Cont1(),
+//                   TextPage(
+//                       text:
+//                           "What are the tools to help you get \n through this?"),
+//                   Button(),
+//                 ],
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//       debugShowCheckedModeBanner: false,
+//     );
+//   }
+// }
+
+class _WorrysState extends State<Worry> {
+  final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
+  final worryData = WorryData();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      key: _globalKey,
+      drawer: const NavigationDrawer(),
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("images/reflect.png"), fit: BoxFit.cover),
+          ),
+          child: Form(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          _globalKey.currentState?.openDrawer();
+                        },
+                        icon: const Icon(Icons.menu, size: FontSize.s40),
+                        color: ColorManager.white,
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, 'homePage');
+                        },
+                        icon: const Icon(Icons.close_sharp, size: FontSize.s40),
+                        color: ColorManager.white,
+                      ),
+                    ],
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 15, bottom: 15),
+                  child: Text(
+                    'Log a Worry',
+                    style: TextStyle(
+                        fontSize: FontSize.s40,
+                        fontWeight: FontWeightManager.bold),
+                  ),
+                ),
+                const SizedBox(height: 60.0),
+                Expanded(
+                  // height: 320,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: worryData.worryList!.length,
+                    physics: const BouncingScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      return SizedBox(
+                        width: 400,
+                        child: ListTile(
+                          title: SizedBox(
+                            height: 350,
+                            child: Container(
+                              // color: ColorManager.black,
+                              width: 350,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30.0),
+                                color: const Color.fromARGB(255, 174, 153, 223),
+                              ),
+                              padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                              child: Column(
+                                // crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 15, left: 10, right: 5),
+                                    child: Center(
+                                      child: Text(
+                                        worryData.worryList![index],
+                                        style: const TextStyle(
+                                            fontSize: FontSize.s20,
+                                            fontWeight:
+                                                FontWeightManager.bold2),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 20,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(15.0),
+                                    child: TextFormField(
+                                      onTap: () {},
+                                      maxLines: 9,
+                                      decoration: InputDecoration(
+                                        fillColor: Colors.white,
+                                        border: OutlineInputBorder(
+                                          // borderSide: BorderSide(
+                                          //   color: Colors.red,
+                                          // ),
+                                          borderRadius:
+                                              BorderRadius.circular(30.0),
+                                        ),
+                                        hintText: 'Type your answer...',
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/worry/worry_data.dart
@@ -0,0 +1,9 @@
+class WorryData {
+  List<String>? worryList = [
+    'Is it that serious?',
+    'Is there a solution?',
+    'Is it out of your control?',
+    'Have you overcome worse?',
+    'What are the tools to help you get\nthrough this?',
+  ];
+}
