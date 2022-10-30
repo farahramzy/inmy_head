b379e677c1548c5fa8337a2eec85720ba5c53eec
farah <farahramzy123@gmail.com>
2022-10-30 16:29:25 +0200

    reflection data

---
+++ lib/reflections/reflection_data.dart
@@ -0,0 +1,9 @@
+class Reflectionata {
+  List<String>? reflectionList = [
+    'What is the most interesting \n thing you learned today?',
+    'What inpspired you today?',
+    'What are your goals for tommorow?',
+    "What is the one thing you could've \n done better today?",
+    'What was your favorite part of the day?',
+  ];
+}
