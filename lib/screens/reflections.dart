034d65e0e67cfb4aba03fa72a7020d349bde3963
haniensami <76820916+haniensami@users.noreply.github.com>
2023-01-04 22:02:03 +0200

    reflections

--- lib/screens/reflections.dart
+++ lib/screens/reflections.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:inmy_head/model/add_reflection.dart';
+import 'package:inmy_head/model/user_model.dart';
+import 'package:inmy_head/widgets/add_reflection_listview.dart';
 // import 'package:inmy_head/reflections/reflections2.dart';
 import '../constants/constants.dart';
 import 'drawer.dart';
@@ -13,6 +18,14 @@ class Reflections extends StatefulWidget {
 class _ReflectionsState extends State<Reflections> {
   final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
   final reflectionData = Reflectionata();
+  final addReflection = AddReflection();
+  List<TextEditingController> controllerList = [
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,76 +82,7 @@ class _ReflectionsState extends State<Reflections> {
                   ),
                 ),
                 const SizedBox(height: 60.0),
-                Expanded(
-                  // height: 320,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: reflectionData.reflectionList!.length,
-                    physics: const BouncingScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      return SizedBox(
-                        width: 400,
-                        child: ListTile(
-                          title: SizedBox(
-                            height: 350,
-                            child: Container(
-                              width: 350,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(30.0),
-                                color: ColorManager.purple3,
-                              ),
-                              padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-                              child: Column(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        top: 15, left: 10, right: 5),
-                                    child: Center(
-                                      child: Text(
-                                        reflectionData.reflectionList![index],
-                                        style: TextStyle(
-                                          color: ColorManager.white,
-                                          fontSize: FontSize.s20,
-                                          fontWeight: FontWeightManager.bold2,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    height: 20,
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(20),
-                                    child: TextFormField(
-                                      onTap: () {},
-                                      maxLines: 9,
-                                      decoration: InputDecoration(
-                                        fillColor: ColorManager.white,
-                                        labelText:
-                                            'Take a few moments reflect...',
-                                        alignLabelWithHint: true,
-                                        labelStyle: TextStyle(
-                                          color: ColorManager.white,
-                                        ),
-                                        enabledBorder: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(10.0),
-                                          borderSide: BorderSide(
-                                              color: ColorManager.white),
-                                        ),
-                                        hintText: 'Type your answer...',
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
+                add_reflection_listview(controllerList: controllerList, reflectionData: reflectionData),
                 SizedBox(
                   height: 40, //height of button
                   width: 100, //width of button
@@ -151,7 +95,27 @@ class _ReflectionsState extends State<Reflections> {
                             borderRadius: BorderRadius.circular(20)),
                         padding: const EdgeInsets.all(10)),
                     onPressed: () {
-                      //save to firebase and go to journal page
+                      addReflection.addReflection(
+                          userId, controllerList.map((e) => e.text).toList());
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: const Text(
+                              'Your reflection has been saved successfully!'),
+                          duration: const Duration(milliseconds: 3000),
+                          width: 300.0,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 15.0,
+                            vertical: 10.0,
+                          ),
+                          behavior: SnackBarBehavior.floating,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                      );
+                      Timer(const Duration(seconds: 3), () {
+                        Navigator.pushNamed(context, 'journal');
+                      });
                     },
                     child: const Text(
                       'Save',
@@ -173,3 +137,4 @@ class _ReflectionsState extends State<Reflections> {
     );
   }
 }
+
