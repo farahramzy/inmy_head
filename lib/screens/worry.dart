0e0a26c0044e020d7fff7db71d6512e6a7e92c6e
farah <farahramzy123@gmail.com>
2023-01-04 18:24:01 +0200

    worry screen

--- lib/screens/worry.dart
+++ lib/screens/worry.dart
@@ -1,11 +1,15 @@
+// ignore_for_file: unused_import
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:inmy_head/data/worry_data.dart';
-
+import 'package:inmy_head/model/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:inmy_head/model/add_worry.dart';
 import '../constants/constants.dart';
+import '../widgets/add_worry_listview.dart';
 import 'drawer.dart';
-// import 'package:inmy_head/reflections/reflections2.dart';
-// import 'constants.dart';
-// import 'navigation_drawer/drawer.dart';
 
 class Worry extends StatefulWidget {
   const Worry({super.key});
@@ -16,7 +20,14 @@ class Worry extends StatefulWidget {
 class _WorrysState extends State<Worry> {
   final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
   final worryData = WorryData();
-
+  final addWorry = AddWorry();
+  List<TextEditingController> controllerList = [
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,88 +83,41 @@ class _WorrysState extends State<Worry> {
                   ),
                 ),
                 const SizedBox(height: 60.0),
-                Expanded(
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: worryData.worryList!.length,
-                    physics: const BouncingScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      return SizedBox(
-                        width: 400,
-                        child: ListTile(
-                          title: SizedBox(
-                            height: 350,
-                            child: Container(
-                              width: 350,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(30.0),
-                                color: ColorManager.purple3,
-                              ),
-                              padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-                              child: Column(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        top: 15, left: 10, right: 5),
-                                    child: Center(
-                                      child: Text(
-                                        worryData.worryList![index],
-                                        style: TextStyle(
-                                            color: ColorManager.white,
-                                            fontSize: FontSize.s20,
-                                            fontWeight:
-                                                FontWeightManager.bold2),
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    height: 20,
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(20),
-                                    child: TextFormField(
-                                      onTap: () {},
-                                      maxLines: 9,
-                                      decoration: InputDecoration(
-                                        fillColor: ColorManager.white,
-                                        labelText:
-                                            'Dive deep into your worries...',
-                                        alignLabelWithHint: true,
-                                        labelStyle: TextStyle(
-                                          color: ColorManager.white,
-                                        ),
-                                        enabledBorder: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(10.0),
-                                          borderSide: BorderSide(
-                                              color: ColorManager.white),
-                                        ),
-                                        hintText: 'Type your answer...',
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
+                WorryListView(
+                    controllerList: controllerList, worryData: worryData),
                 SizedBox(
                   height: 40, //height of button
                   width: 100, //width of button
                   child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                        primary: ColorManager.darkPurple,
+                        backgroundColor: ColorManager.darkPurple,
                         side: BorderSide(
                             width: 3, color: ColorManager.darkPurple),
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(20)),
                         padding: const EdgeInsets.all(10)),
                     onPressed: () {
-                      //save to firebase and go to journal page
+                      addWorry.addWorry(
+                          userId, controllerList.map((e) => e.text).toList());
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: const Text(
+                              'Your worry has been saved successfully!'),
+                          duration: const Duration(milliseconds: 3000),
+                          width: 300.0,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 15.0,
+                            vertical: 10.0,
+                          ),
+                          behavior: SnackBarBehavior.floating,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                      );
+                      Timer(const Duration(seconds: 3), () {
+                        Navigator.pushNamed(context, 'journal');
+                      });
                     },
                     child: const Text(
                       'Save',
