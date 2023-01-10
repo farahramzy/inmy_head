3d562f036768f07037869ee6e525d380a860ddd6
haniensami <76820916+haniensami@users.noreply.github.com>
2023-01-11 00:35:57 +0200

    push_notification

--- lib/screens/journal.dart
+++ lib/screens/journal.dart
@@ -1,11 +1,51 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:inmy_head/data/journal_data.dart';
+import 'package:inmy_head/firebase_options.dart';
 import '../constants/constants.dart';
 import '../widgets/journal/lets_reflect_listview.dart';
 import '../widgets/journal/screen_text.dart';
 import '../widgets/journal/tab_view.dart';
 import '../widgets/journal/username_text.dart';
 import 'drawer.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+ 
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+
+void notification() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+
+  // NotificationSettings settings = await messaging.requestPermission(
+  //   alert: true,
+  //   announcement: false,
+  //   badge: true,
+  //   carPlay: false,
+  //   criticalAlert: false,
+  //   provisional: false,
+  //   sound: true,
+  // );
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+  runApp(const Journal());
+}
 
 class Journal extends StatefulWidget {
   const Journal({super.key});
