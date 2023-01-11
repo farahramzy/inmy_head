dc4314bb44380d470b39bce37a0a6d14d8a4ec79
farah <farahramzy123@gmail.com>
2023-01-11 16:06:40 +0200

    constants file final update

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static Color primaryColor = const Color.fromARGB(255, 81, 83, 161);
+  static Color secondryColor = const Color.fromARGB(255, 6, 6, 6);
+  static Color purple1 = const Color(0xff8280D6);
+  static Color purple2 = const Color.fromARGB(255, 155, 123, 210);
+  static Color purple3 = const Color(0xFF565797);
+  static Color darkblue = const Color.fromARGB(255, 84, 18, 182);
+  static Color white = const Color.fromARGB(255, 255, 255, 255);
+  static Color beige = const Color(0xFFECE3DD);
+  static Color beigYellow = const Color(0xffefc7af);
+  static Color blue = Colors.blue;
+  static Color grey = Colors.grey;
+}
---
+++ lib/constants/font_constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FontWeightManager {
+  static const FontWeight w800 = FontWeight.w800;
+  static const FontWeight bold = FontWeight.bold;
+  static const FontWeight bold2 = FontWeight.w900;
+}
+
+class FontSize {
+  static const double s20 = 20;
+  static const double s15 = 15;
+  static const double s18 = 18;
+  static const double s25 = 25.0;
+  static const double s30 = 30.0;
+  static const double s35 = 35.0;
+  static const double s40 = 40.0;
+  static const double s70 = 70.0;
+}
