13a51db2b77b46b842b135692b9b8a0331945d5d
Farah Khaled <farah1900784@miuegypt.edu.eg>
2023-01-12 20:39:08 +0200

    admin edit reflections

--- lib/screens/edit_reflect.dart
+++ lib/screens/edit_reflect.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../constants/color_constants.dart';
@@ -26,148 +27,164 @@ class _EditReflectionState extends State<EditReflection> {
                 image: AssetImage("images/3.png"), fit: BoxFit.cover),
           ),
           child: Form(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          //_globalKey.currentState?.openDrawer();
-                        },
-                        icon: const Icon(Icons.menu, size: FontSize.s40),
-                        color: ColorManager.secondryColor,
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, 'welcome');
-                        },
-                        icon: const Icon(Icons.close_sharp, size: FontSize.s40),
-                        color: ColorManager.secondryColor,
-                      ),
-                    ],
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            //_globalKey.currentState?.openDrawer();
+                          },
+                          icon: const Icon(Icons.menu, size: FontSize.s40),
+                          color: ColorManager.secondryColor,
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, 'welcome');
+                          },
+                          icon:
+                              const Icon(Icons.close_sharp, size: FontSize.s40),
+                          color: ColorManager.secondryColor,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 15, bottom: 15),
-                  child: Text(
-                    'Edit Reflections',
-                    style: TextStyle(
-                        color: ColorManager.secondryColor,
-                        fontSize: FontSize.s40,
-                        fontWeight: FontWeightManager.bold),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15, bottom: 15),
+                    child: Text(
+                      'Edit Reflections',
+                      style: TextStyle(
+                          color: ColorManager.secondryColor,
+                          fontSize: FontSize.s40,
+                          fontWeight: FontWeightManager.bold),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 10.0),
-                Consumer(
-                  builder: (_, ref, __) {
-                    return ref.watch(reflectionQuestionsProvider).when(
-                      data: (value) {
-                        return SizedBox(
-                          height: 460,
-                          child: ListView.builder(
-                              physics: const BouncingScrollPhysics(),
-                              scrollDirection: Axis.vertical,
-                              shrinkWrap: true,
-                              itemCount: value.docs.length,
-                              itemBuilder: (context, index) {
-                                return Column(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 8.0),
-                                      child: SizedBox(
-                                        height: 100,
-                                        child: Container(
-                                          width: 400,
-                                          decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(35.0),
-                                            color: const Color.fromARGB(
-                                                255, 176, 155, 203),
-                                          ),
-                                          child: Padding(
-                                            padding: const EdgeInsets.symmetric(
-                                                vertical: 8.0,
-                                                horizontal: 10.0),
-                                            child: TextFormField(
-                                                maxLines: 2,
-                                                initialValue: value.docs[index]
-                                                    .get('text'),
-                                                decoration: InputDecoration(
-                                                  hintText:
-                                                      'Enter your reflection',
-                                                  hintStyle: TextStyle(
+                  const SizedBox(height: 10.0),
+                  Consumer(
+                    builder: (_, ref, __) {
+                      return ref.watch(reflectionQuestionsProvider).when(
+                        data: (value) {
+                          return SizedBox(
+                            height: 500,
+                            width: double.infinity,
+                            child: ListView.builder(
+                                physics: const BouncingScrollPhysics(),
+                                scrollDirection: Axis.horizontal,
+                                shrinkWrap: true,
+                                itemCount: value.docs.length,
+                                itemBuilder: (context, index) {
+                                  ///////Tik Tik
+                                  String newText =
+                                      value.docs[index].get('text');
+                                  return Column(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 10.0),
+                                        child: SizedBox(
+                                          height: 300,
+                                          child: Container(
+                                            width: 360,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(35.0),
+                                              color: const Color.fromARGB(
+                                                  255, 176, 155, 203),
+                                            ),
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 8.0,
+                                                      horizontal: 10.0),
+                                              child: TextFormField(
+                                                  maxLines: 2,
+                                                  initialValue: value
+                                                      .docs[index]
+                                                      .get('text'),
+                                                  decoration: InputDecoration(
+                                                    hintText:
+                                                        'Enter your reflection',
+                                                    hintStyle: TextStyle(
+                                                        color: ColorManager
+                                                            .secondryColor,
+                                                        fontSize: FontSize.s20,
+                                                        fontWeight:
+                                                            FontWeightManager
+                                                                .bold2),
+                                                  ),
+                                                  style: TextStyle(
                                                       color: ColorManager
                                                           .secondryColor,
                                                       fontSize: FontSize.s20,
                                                       fontWeight:
                                                           FontWeightManager
                                                               .bold2),
-                                                ),
-                                                style: TextStyle(
-                                                    color: ColorManager
-                                                        .secondryColor,
-                                                    fontSize: FontSize.s20,
-                                                    fontWeight:
-                                                        FontWeightManager
-                                                            .bold2),
 
-                                                // onChanged: (value) {
-                                                //   // reflectionData.reflection = value;
-                                                // },
+                                                  // onChanged: (value) {
+                                                  //   // reflectionData.reflection = value;
+                                                  // },
 
-                                                // update automatically on changes without needing a button
-                                                onChanged: (newValue) => value
-                                                    .document(value
-                                                        .docs[index].documentID)
-                                                    .updateData(
-                                                        {'text': newValue})),
+                                                  // update automatically on changes without needing a button
+
+                                                  // Tik Tik
+                                                  onChanged: (newVal) {
+                                                    newText = newVal;
+                                                  }),
+                                            ),
                                           ),
                                         ),
                                       ),
-                                    ),
-                                    // Expanded(
-                                    // child:
-                                    Align(
-                                      alignment: Alignment.center,
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(16.0),
-                                        child: ElevatedButton(
-                                          onPressed: () {
-                                            // perform desired action when button is pressed
-                                          },
-                                          child: const Text('Save'),
+                                      Align(
+                                        alignment: Alignment.center,
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(16.0),
+                                          child: ElevatedButton(
+                                            onPressed: () {
+                                              print(value.docs[index].id);
+                                              print(newText);
+                                              FirebaseFirestore.instance
+                                                  .collection(
+                                                      "Reflection Question")
+                                                  .doc(value.docs[index].id)
+                                                  .set({'text': newText});
+                                            },
+                                            child: const Text('Save'),
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                    // )
-                                  ],
-                                );
-                              }),
-                          //SAVE BUTTON to reload the page
-                          //how to code a save button that reloads page on
+                                    ],
+                                  );
+                                }),
 
-                          // ElevatedButton(
-                          //     onPressed: (context) => Navigator.pop(context),
-                          //     child: const Text("save")
-                          //     )
+                            // )
+                            //SAVE BUTTON to reload the page
+                            //how to code a save button that reloads page on
 
-                          //how make a button?
-                        );
-                      },
-                      error: (Object error, StackTrace err) {
-                        return const Text("Error loading your questions");
-                      },
-                      loading: () {
-                        return const Center(child: CircularProgressIndicator());
-                      },
-                    );
-                  },
-                )
-              ],
+                            // ElevatedButton(
+                            //     onPressed: (context) => Navigator.pop(context),
+                            //     child: const Text("save")
+                            //     )
+
+                            //how make a button?
+                          );
+                        },
+                        error: (Object error, StackTrace err) {
+                          print(err.toString());
+                          return const Text("Error loading your questions");
+                        },
+                        loading: () {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        },
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
